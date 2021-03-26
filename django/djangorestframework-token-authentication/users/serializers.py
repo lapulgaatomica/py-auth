1f69cffe9a519cbdb90241ad7526c608ff14ef53
pulga <akindeleodedoyin@gmail.com>
2021-03-26 22:44:07 +0100

    login url added

--- django/djangorestframework-token-authentication/users/serializers.py
+++ django/djangorestframework-token-authentication/users/serializers.py
@@ -1,9 +1,15 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-# from .models import CustomUser
+from .models import CustomUser
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length=68, min_length=6, write_only=True)
+
     class Meta:
         model = get_user_model()
-        fields = ['email', 'username', 'password']
+        fields = ('email', 'username', 'password')
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
